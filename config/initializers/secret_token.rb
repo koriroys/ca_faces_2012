5dbdf34b03e6f2bb2ae6c53542edbefc15c4ca67
Kori Roys <kori@koriroys.com>
2012-01-24 14:47:56 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dontsuck::Application.config.secret_token = '7c084bb255e7680e3be7062118dc7077fd803d26e0192ef0d5980b60bca84293028ede57ad10c2ccf63a854b05e3c3873a6bb544f2a02850ae56256468dfc920'
