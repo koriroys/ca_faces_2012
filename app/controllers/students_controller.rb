452772edf0e3a3fe6677a9b0724ffc442202e7a8
Kori Roys <kori@koriroys.com>
2012-01-24 19:25:32 -0600

    Jon Buda added search function

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,12 @@
 class StudentsController < ApplicationController
   
   def index
-    @students = Student.all
+    if params[:class_day].present?
+      @students = Student.where(:class_day => params[:class_day])
+      @students.reverse!
+    else
+      @students = Student.all
+    end
   end
   
   def show
